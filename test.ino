9c1ce10c35ded6cbf6d7462ced1796bb9b0a735d
Rency Kansagra <123216794+uta-rxk4931@users.noreply.github.com>
2025-04-16 15:35:58 -0500

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,154 +1,117 @@
-//This is the current loaded file.
-/*
-DIP switches on Sabertooth:
-DIP 1: OFF && DIP 2: ON  => R/C Control Mode
-DIP 3: ON => Power Supply Mode (May be set to OFF with battery)
-DIP 4: OFF => Independent Mode (S1 = M1, S2 = M2)
-DIP 5: ON => Linear Control
-DIP 6: OFF => Microcontroller Mode
-*/
-
-// include the Servo library
 #include <Servo.h>
 
+// Motor command definitions
+#define FORWARD 'f'
+#define STOP 's'
 
 #define DELAY 10
+#define ACCEL_STEP 2
+#define DECEL_STEP 25
+#define MAX_SPEED 2000
+#define NEUTRAL_SPEED 1500
+#define MIN_SPEED 1000
 
-uint8_t FORWARD = 1;
-uint8_t BACKWARD = 2;
-uint8_t LEFT = 3;
-uint8_t RIGHT = 4;
-uint8_t QUIT = 5;
-uint8_t STOP = 6;
+// Servo motor objects
+Servo FrontLeftMotor;
+Servo FrontRightMotor;
+Servo BackLeftMotor;
+Servo BackRightMotor;
 
-Servo FrontLeftMotor;  // create a servo object
-Servo FrontRightMotor; // create a servo object
-Servo BackLeftMotor;   // create a servo object
-Servo BackRightMotor;  // create a servo object
+// Movement states
+enum MovementPhase { IDLE, ACCELERATE, CRUISE, DECELERATE };
+MovementPhase phase = IDLE;
 
-int speed = 1500;
-int f_speed = 1500;
-int b_speed = 1500;
-int acceleration = 2;
-int deceleration = 25;
-//std::string incoming_data();
+char currentCommand = '\0'; // '\0' means no command
+unsigned long cruiseStartTime = 0;
+unsigned long cruiseDuration = 0;
+int curSpeed = NEUTRAL_SPEED;
 
-void setup()
-{
-  Serial.begin(9600);         
-  FrontLeftMotor.attach(9);   
-  FrontRightMotor.attach(10); 
-  BackLeftMotor.attach(3);    
-  BackRightMotor.attach(4);   
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  Serial.begin(9600);
+  FrontLeftMotor.attach(9);
+  FrontRightMotor.attach(10);
+  BackLeftMotor.attach(3);
+  BackRightMotor.attach(4);
 
-  // Set all motors to neutral speed initially
-  FrontLeftMotor.writeMicroseconds(1500);
-  FrontRightMotor.writeMicroseconds(1500);
-  BackLeftMotor.writeMicroseconds(1500);
-  BackRightMotor.writeMicroseconds(1500);
+  stopAllMotors();
+  Serial.println("System Ready");
 }
 
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    String incoming_data(Serial.readString());
+void loop() {
+  // Read and parse incoming command
+  if (Serial.available() > 0) {
+    String incoming_data = Serial.readStringUntil('\n');
     char t_command = incoming_data.charAt(0);
     incoming_data.remove(0, 1);
     int t_delay = incoming_data.toInt();
-    if (t_command == 'f')
-    {
-      for (speed = 1500; speed < 2000; speed += acceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);
-        FrontRightMotor.writeMicroseconds(speed);
-        BackLeftMotor.writeMicroseconds(speed);
-        BackRightMotor.writeMicroseconds(speed);
-        delay(DELAY);
-      }
-      delay(t_delay);
-      for (speed = 2000; speed > 1500; speed -= deceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);
-        FrontRightMotor.writeMicroseconds(speed);
-        BackLeftMotor.writeMicroseconds(speed);
-        BackRightMotor.writeMicroseconds(speed);
-        delay(DELAY);
-      }
-      //delay(1000);
+
+    if (t_command == FORWARD) {
+      currentCommand = FORWARD;
+      cruiseDuration = t_delay;
+      curSpeed = NEUTRAL_SPEED;
+      phase = ACCELERATE;
+      Serial.println("FORWARD command received");
     }
-    else if (t_command == "b")
-    {
-      for (speed = 1500; speed > 1000; speed -= acceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);
-        FrontRightMotor.writeMicroseconds(speed);
-        BackLeftMotor.writeMicroseconds(speed);
-        BackRightMotor.writeMicroseconds(speed);
-        delay(DELAY);
-      }
-      delay(t_delay);
-      for (speed = 1000; speed < 1500; speed += deceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);
-        FrontRightMotor.writeMicroseconds(speed);
-        BackLeftMotor.writeMicroseconds(speed);
-        BackRightMotor.writeMicroseconds(speed);
-        delay(DELAY);
-      }
-      //delay(1000);
-    }
-    else if (t_command == "r") //changed from left to right just to test
-    {
-      // CHANGED: Implemented omniwheel strafing RIGHT
-      for (speed = 1500; speed < 2000; speed += acceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(1500 - (speed - 1500));  // reverse
-        FrontRightMotor.writeMicroseconds(speed);                 // forward
-        BackLeftMotor.writeMicroseconds(speed);                   // forward
-        BackRightMotor.writeMicroseconds(1500 - (speed - 1500));  // reverse
-        delay(DELAY);
-      }
-      delay(t_delay);
-      for (speed = 2000; speed > 1500; speed -= deceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(1500 - (speed - 1500));  // reverse
-        FrontRightMotor.writeMicroseconds(speed);                 // forward
-        BackLeftMotor.writeMicroseconds(speed);                   // forward
-        BackRightMotor.writeMicroseconds(1500 - (speed - 1500));  // reverse 
-        delay(DELAY);
-      }
-      //delay(1000);
-    }
-    else if (t_command == "l") //changed from right to left just to test
-    {
-      // CHANGED: Implemented omniwheel strafing LEFT
-      for (speed = 1500; speed < 2000; speed += acceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);                   // forward
-        FrontRightMotor.writeMicroseconds(1500 - (speed - 1500));  // reverse
-        BackLeftMotor.writeMicroseconds(1500 - (speed - 1500));    // reverse
-        BackRightMotor.writeMicroseconds(speed);                   // forward
-        delay(DELAY);
-      }
-      delay(t_delay);
-      for (speed = 2000; speed > 1500; speed -= deceleration)
-      {
-        FrontLeftMotor.writeMicroseconds(speed);                   
-        FrontRightMotor.writeMicroseconds(1500 - (speed - 1500));  
-        BackLeftMotor.writeMicroseconds(1500 - (speed - 1500));    
-        BackRightMotor.writeMicroseconds(speed);   
-        delay(DELAY);
-      }
-      //delay(1000);
-    }
-    else if (t_command == "s")
-    {
-      FrontLeftMotor.writeMicroseconds(1500);
-      FrontRightMotor.writeMicroseconds(1500);
-      BackLeftMotor.writeMicroseconds(1500);
-      BackRightMotor.writeMicroseconds(1500);
+    else if (t_command == STOP) {
+      currentCommand = '\0';
+      phase = IDLE;
+      stopAllMotors();
+      Serial.println("STOP received");
     }
   }
+
+  // Handle movement logic
+  if (currentCommand == FORWARD) {
+    switch (phase) {
+      case ACCELERATE:
+        if (curSpeed < MAX_SPEED) {
+          curSpeed += ACCEL_STEP;
+          if (curSpeed >= MAX_SPEED) {
+            curSpeed = MAX_SPEED;
+            phase = CRUISE;
+            cruiseStartTime = millis();  // Start cruise timer *after* acceleration
+            Serial.println("Reached max speed, starting cruise");
+          }
+          setAllMotors(curSpeed);
+          delay(DELAY);
+        }
+        break;
+
+      case CRUISE:
+        setAllMotors(MAX_SPEED);
+        if (millis() - cruiseStartTime >= cruiseDuration) {
+          phase = DECELERATE;
+          Serial.println("Cruise complete, starting deceleration");
+        }
+        break;
+
+      case DECELERATE:
+        if (curSpeed > NEUTRAL_SPEED) {
+          curSpeed -= DECEL_STEP;
+          if (curSpeed <= NEUTRAL_SPEED) {
+            curSpeed = NEUTRAL_SPEED;
+            phase = IDLE;
+            currentCommand = '\0';
+            Serial.println("Deceleration complete");
+          }
+          setAllMotors(curSpeed);
+          delay(DELAY);
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+void stopAllMotors() {
+  setAllMotors(NEUTRAL_SPEED);
+}
+
+void setAllMotors(int pwm) {
+  FrontLeftMotor.writeMicroseconds(pwm);
+  FrontRightMotor.writeMicroseconds(pwm);
+  BackLeftMotor.writeMicroseconds(pwm);
+  BackRightMotor.writeMicroseconds(pwm);
 }
